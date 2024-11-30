1935e0761dd0868670cbcb2f3f9123f1dd53b61c
DreamAndMagicClub <3584938246@qq.com>
2024-11-30 21:19:16 +0800

    Site updated: 2024-11-30 21:19:16

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732971424139";
+const VERSION = "1732972756164";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
